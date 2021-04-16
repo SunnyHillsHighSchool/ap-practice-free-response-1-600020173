3cba1df1a88ed8363f4c2979f4c63ef202cb09f5
Jonathan Hsing <>
2021-04-16 22:50:48 +0000

    added getDelimitersList and isBalanced

--- Delimiters.java
+++ Delimiters.java
@@ -1,3 +1,4 @@
+//written by Jonathan Hsing
 import java.util.*;
 
 public class Delimiters
@@ -13,11 +14,40 @@ public class Delimiters
 
 	public ArrayList<String> getDelimitersList(String[] tokens)
 	{
-		return null;
+//create an arrayList
+ArrayList<String> a = new ArrayList<String>();
+//For each loop
+for(String b : tokens){
+//if a is open or closed delimiter, add it to the list
+if(b.equals(openDel) || b.equals(closeDel))
+a.add(b);
+}
+//return a
+return a;
 	}
 
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
-		return false;
+		//create a counter variable
+int c = 0;
+//for each loop
+for(String a: delimiters)
+{
+//if c < 0, return false
+if(c < 0)
+return false;
+// if a is open, increase c 
+if(a.equals(openDel))
+c++;
+//if a is closed, decrese c
+if(a.equals(closeDel))
+c--;
+}
+//if c = 0, return true
+if(c == 0)
+return true;
+//otherwise return false
+else
+return false;
 	}
 }
